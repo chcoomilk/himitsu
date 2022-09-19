e724e27a615ed539edd5dba4c34e268a7002966d
chcoomilk <klaustin.wijaya@gmail.com>
2022-09-19 12:33:34 +0700

    maybe

--- vite.config.ts
+++ vite.config.ts
@@ -32,13 +32,13 @@ export default ({ mode }) => {
         experimental: {
             renderBuiltUrl(filename, { hostId, type }) {
                 if (type === 'public') {
-                    return { runtime: `window.__baseUrl(${JSON.stringify(filename)})` };
+                    return { relative: false };
                 }
                 else if (path.extname(hostId) === '.js') {
                     return { runtime: `window.__assetsPath(${JSON.stringify(filename)})` };
                 }
                 else {
-                    return { runtime: `window.__toCdnUrl(${JSON.stringify(filename)})` };
+                    return { relative: true };
                 }
             }
         },
